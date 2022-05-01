e7542663fc82c825690cfd367e97726f1d7e103d
Plaksin Aleksander <plaksin.aleksander@alphaopen.com>
2022-05-01 11:33:55 +0300

    create 3d playground

---
+++ Assets/RelativeMovement.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float rotationSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
+    private ControllerColliderHit _contact;
+    private CharacterController _characterController;
+    private Animator _animator;
+
+    void Start() {
+        _vertSpeed = minFall;
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        if (horInput != 0 || vertInput != 0) {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotationSpeed * Time.deltaTime);
+        }
+        bool hitGround = false;
+        RaycastHit hit;
+        if (_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit)) {
+            float check = (_characterController.height + _characterController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+        _animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (hitGround) {
+            if (Input.GetButton("Jump")) {
+                _vertSpeed = jumpSpeed;
+            }
+            else {
+                _vertSpeed = minFall;
+                _animator.SetBool("Jumping", false);
+            }
+        }
+        else {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocity) {
+                _vertSpeed = terminalVelocity;
+            }
+            if (_contact != null) {
+                _animator.SetBool("Jumping", true);
+            }
+            if (_characterController.isGrounded) {
+                if (Vector3.Dot(movement, _contact.normal) < 0) {
+                    movement = _contact.normal * moveSpeed;
+                } else {
+                    movement += _contact.normal * moveSpeed;
+                }
+            }
+        }
+        movement.y = _vertSpeed;
+        movement *= Time.deltaTime;
+        _characterController.Move(movement);
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+        _contact = hit;
+    }
+}
